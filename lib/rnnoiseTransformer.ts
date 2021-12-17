88b996d7ad315ae38f3d826931c50a4fb784e863
KOUNOIKE Yuusuke <kounoike.yuusuke@gmail.com>
2021-12-18 02:32:20 +0900

    add comment

--- lib/rnnoiseTransformer.ts
+++ lib/rnnoiseTransformer.ts
@@ -22,7 +22,12 @@ export function getRnnoiseTransformer (
       buffer = new Float32Array(audioData.numberOfFrames)
     }
     audioData.copyTo(buffer, opts)
+
+    // 比較用
     const buffer2 = new Float32Array(buffer)
+
+    // Rnnoiseが480フレームしか処理できないからホントは区切って渡さないといけないんだけど
+    // たまたまBreakout boxも480フレームで渡してくるからサボってそのままにしている
     const vad = rnnoise.processFrame(buffer)
     const newAudioData = new AudioData({
       format: audioData.format,
@@ -32,6 +37,8 @@ export function getRnnoiseTransformer (
       timestamp: audioData.timestamp,
       data: buffer
     })
+
+    // 比較表示
     console.log(buffer, buffer2)
     controller.enqueue(newAudioData)
   }
