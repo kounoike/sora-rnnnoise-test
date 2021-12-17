3550be48963d782cb0d341b4f8174b4d96ae13c9
KOUNOIKE Yuusuke <kounoike.yuusuke@gmail.com>
2021-12-18 02:30:25 +0900

    prettier

--- lib/rnnoiseTransformer.ts
+++ lib/rnnoiseTransformer.ts
@@ -1,15 +1,22 @@
-import {Rnnoise} from '@shiguredo/rnnoise-wasm'
+import { Rnnoise } from '@shiguredo/rnnoise-wasm'
 
-
-export function getRnnoiseTransformer (rnnoise: Rnnoise): (audioData: AudioData, controller:TransformStreamDefaultController<AudioData>) => void {
+export function getRnnoiseTransformer (
+  rnnoise: Rnnoise
+): (
+  audioData: AudioData,
+  controller: TransformStreamDefaultController<AudioData>
+) => void {
   let buffer = new Float32Array()
-  return (audioData: AudioData, controller: TransformStreamDefaultController<AudioData>) => {
+  return (
+    audioData: AudioData,
+    controller: TransformStreamDefaultController<AudioData>
+  ) => {
     // console.log(
     //   'audioData',
     //   audioData,
     //   rnnoise
     // )
-    const opts = {planeIndex: 0}
+    const opts = { planeIndex: 0 }
     const size = audioData.numberOfFrames
     if (buffer.length < size) {
       buffer = new Float32Array(audioData.numberOfFrames)
@@ -19,13 +26,13 @@ export function getRnnoiseTransformer (rnnoise: Rnnoise): (audioData: AudioData,
     const vad = rnnoise.processFrame(buffer)
     const newAudioData = new AudioData({
       format: audioData.format,
-        numberOfChannels: audioData.numberOfChannels,
-        numberOfFrames: audioData.numberOfFrames,
-        sampleRate: audioData.sampleRate,
-        timestamp: audioData.timestamp,
-        data: buffer
-      })
-      console.log(buffer, buffer2)
+      numberOfChannels: audioData.numberOfChannels,
+      numberOfFrames: audioData.numberOfFrames,
+      sampleRate: audioData.sampleRate,
+      timestamp: audioData.timestamp,
+      data: buffer
+    })
+    console.log(buffer, buffer2)
     controller.enqueue(newAudioData)
   }
 }
